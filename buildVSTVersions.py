f5ec49611642d4c2eb9a3eed79c8e0adc749faec
Oli Larkin <olilarkin@googlemail.com>
2014-11-23 12:47:29 +0000

    update buildvst script

--- buildVSTVersions.py
+++ buildVSTVersions.py
@@ -1,4 +1,6 @@
-import subprocess, glob
+import subprocess, glob, shutil
+
+vstdir = "/Library/Audio/Plug-Ins/VST/MyFaust"
 
 faustFiles = glob.glob("*.dsp")
 
@@ -6,3 +8,10 @@ for file in faustFiles:
   if(file != "IIRHilbert.dsp"):
     command = ["faust2vst", file]
     subprocess.call(command)
+    
+vstFiles = glob.glob("*.vst")
+
+for file in vstFiles:
+  outputfile = vstdir + "/" + file
+  shutil.rmtree(outputfile)
+  shutil.move(file, outputfile)
